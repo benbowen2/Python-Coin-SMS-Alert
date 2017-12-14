942e2004ee63111303cd4a6bc0c10e8eadad97f2
Ben Bowen <ben.bowen2@gmail.com>
2017-12-14 18:22:08 -0500

    added more config vars

--- alert.py
+++ alert.py
@@ -8,23 +8,6 @@ s = sched.scheduler(time.time, time.sleep)
 
 print("https://api.coinmarketcap.com/v1/ticker/%s/" % config.coin)
 
-def checkPrice(sc): 
-	#check API
-	results = json.loads(urllib2.urlopen("https://api.coinmarketcap.com/v1/ticker/%s/" % config.coin).read())
-	
-	print("%f - %f " %(float(results[0]['price_btc']), float(config.minimum)))
-
-	if float(results[0]['price_btc']) < float(config.minimum):
-		sendSMS("WARNING: %s is below %s" %(config.coin, config.minimum))
-	else:
-		print("we good")
-
-	s.enter(10, 1, checkPrice, (sc,))
-
-s.enter(10, 1, checkPrice, (s,))
-s.run()
-
-
 def sendSMS(body):
 	client = Client(config.account_sid, config.auth_token)
 	message = client.messages.create(
@@ -33,5 +16,22 @@ def sendSMS(body):
 		body = body)
 
 	print (message.sid)
+
+def checkPrice(sc): 
+	#check API
+	results = json.loads(urllib2.urlopen("https://api.coinmarketcap.com/v1/ticker/%s/" % config.coin).read())
+	
+	print("%f - %f " %(float(results[0]['price_btc']), float(config.minimum)))
+
+	if float(results[0]["price_%s" %config.price_type]) < float(config.minimum):
+		body = "WARNING: %s is below %s" %(config.coin, config.minimum)
+		sendSMS(body)
+	else:
+		print("we good")
+
+	s.enter(10, 1, checkPrice, (sc,))
+
+s.enter(10, 1, checkPrice, (s,))
+s.run()
 
 
--- config-example.py
+++ config-example.py
@@ -6,3 +6,8 @@ auth_token = ""
 sms_to = "+1XXXXXXXXXX"
 sms_from_ = "+1XXXXXXXXXX"
 
+#currency checked on coinmarketcap.com
+coin = "ripple"
+minimum = "0.000048"
+price_type = "btc"
+
